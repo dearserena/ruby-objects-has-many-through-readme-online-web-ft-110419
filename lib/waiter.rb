042537065ec0ac73c60c3a604902e9da7c624d69
serena Lee <dearserenalee@gmail.com>
2019-12-09 16:03:52 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,15 @@
 class Waiter
-
+attr_accessor :name, :years 
+  
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @years = years 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all
+  end
 end
