84fa872f696ff907e2ae44336b90c7e27f4d7f08
serena Lee <dearserenalee@gmail.com>
2019-12-09 16:03:44 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,15 +1,3 @@
 class Meal
-attr_accessor :name, :years 
-  
-  @@all = []
-  
-  def initialize(name, age)
-    @name = name
-    @years = years 
-    @@all << self 
-  end 
-  
-  def self.all 
-    @@all
-  end 
+ 
 end
