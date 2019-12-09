062446c14d64dd213616502e0bac0f9d41dff61a
serena Lee <dearserenalee@gmail.com>
2019-12-09 16:22:29 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -17,16 +17,6 @@ class Customer
     Meal.new(waiter, self, total, tip)
   end
   
-  def meals
-  Meal.all.select do |meal|
-    meal.customer == self
-  end
-  
-  def waiters
-  meals.map do |meal|
-    meal.waiter
-  end
-end
   
 end
 end
